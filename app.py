0224a23c42496ecae18d492d83afcbfeb3b03124
chimodo <110926964+chimodo@users.noreply.github.com>
2024-11-19 01:21:55 +0000

    The basic html form is now complete and functional apart from the validation. added onset date and severity options, added skeletons for required functions, created a separate file called script.js for javascript code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ health_data = {} # dictionary will store responses
 @app.route('/', methods = ['GET','POST'])
 def authenticate():
     if request.method == "POST":
-        info = request.form.to_dict()
+        info = request.form.to_dict(flat=False) # flat = false allows our dictionary to be nested therefore all checkbox choices will be returned
         # info dictionary format {'name': 'Alice', 'surname': 'Smith', 'email': 'Alice@example.com'}
         print(info)
         if find_user(info): # passed into find user function which looks for the name
@@ -21,8 +21,8 @@ def authenticate():
 def get_health_data():
     # Example list of risk factors; you can modify this as needed.
     if request.method == "POST":
-        health_data = request.form 
-        visit_reason = request.form.getlist('reason[]') # asked chatgpt how to access the list of checkbox results
+        health_data = request.form.to_dict(flat = False)
+        #visit_reason = request.form.getlist('reason[]') # asked chatgpt how to access the list of checkbox results
         #health_data['reason']
         # request form gets all responses from the form. to dict converts it to dictionary
         #print(health_data) # checking if appending to dict successful
@@ -41,6 +41,12 @@ def find_user(info):
     # TODO use info in the dictionary, look for a line with the same name, surname and email. All 3 should match
     # info dictionary format {'name': 'Alice', 'surname': 'Smith', 'email': 'Alice@example.com'}
     return True # change back to false later
+
+def create_file(health_data):
+    # append patient details from scheduling to json file
+    # you can use the jsonfy function
+    # save it to separate folder
+    pass # temporarily ignore this function
 
 if __name__ == '__main__':
     app.run(debug=True)
